d8f6c3cedc228e647dc9976760f726dcea717963
kurniawan86 <90589478+kurniawan86@users.noreply.github.com>
2021-11-09 16:52:17 +0700

    add main.py and change aditor to pycharm

--- data.py
+++ data.py
@@ -7,34 +7,31 @@ Created on Wed Aug 18 14:17:24 2021
 import pandas as panda
 
 class classData:
-    __data=None
+    __data = None
     
     def __init__(self):
         self.__readExcel()
     
-    def getData(self):
-        return self.__data
-    
-    def getDataI(self,i):
-        print(self.__data[i])
-    
-    def getDist(self,i,j):
-        # i index data ke-1
-        # j index data ke-2
-        data=self.__data
-        data1=data[i]
-        data2=data[j]
-        nFitur=len(data1)
-        dist=0
-        for k in range (nFitur):
-            dist=dist+(abs(data1[k]-data2[k]))
-        return dist
+    # def getData(self):
+    #     return self.__data
+    #
+    # def getDataI(self,i):
+    #     print(self.__data[i])
+    #
+    # def getDist(self,i,j):
+    #     # i index data ke-1
+    #     # j index data ke-2
+    #     data=self.__data
+    #     data1=data[i]
+    #     data2=data[j]
+    #     nFitur=len(data1)
+    #     dist=0
+    #     for k in range (nFitur):
+    #         dist=dist+(abs(data1[k]-data2[k]))
+    #     return dist
     
     def __readExcel(self):
-        file=panda.read_excel(open('data.xlsx','rb'))
-        df=panda.DataFrame(file,columns=(['x','y']))
-        dataset=df.values.tolist()
-        self.__data=dataset
-
-# cd=classData()
-# print(cd.getDist(1, 2))
+        file = panda.read_excel(open('data.xlsx','rb'))
+        df = panda.DataFrame(file,columns=(['x','y']))
+        dataset = df.values.tolist()
+        self.__data = dataset
